92b6cdf91f30dbb08d3f8de5c78e21a78481cfc0
Joshua <joshuanazareth97@gmail.com>
2022-03-25 01:47:59 +0530

    Add material ui theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
+import { ThemeProvider } from "@mui/material/styles";
 import ProtectedRoute from "components/ProtectedRoute/ProtectedRoute";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routeList } from "routes";
+import { theme } from "Theme";
 import "./App.css";
 
 function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContent />
+    </ThemeProvider>
+  );
+}
+
+const AppContent = () => {
   return (
     <BrowserRouter>
       <Routes>
@@ -27,6 +37,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
